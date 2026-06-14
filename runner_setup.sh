27815deef2b4a8acab2e84292bb89d1a8d20c177
sergeibahuser <sergeibahankov@gmail.com>
2026-06-15 00:31:44 +0300

    Add runner_setup.sh

---
+++ runner_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# runner_setup.sh
+# Инструкция: создайте self-hosted runner в GitHub, скачайте пакет на сервер и зарегистрируйте runner.
+echo "Follow GitHub docs to register a self-hosted runner and assign label: linux-build"
